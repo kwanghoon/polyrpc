7cba1c2bd1f86a9db6d895a3def05f6c5c7ae0aa
Kwanghoon Choi <lazyswamp@gmail.com>
2020-06-30 14:48:08 +0900

    Initialized the structure of erase.hs

--- app/Erase.hs
+++ app/Erase.hs
@@ -2,8 +2,15 @@
 
 module Erase where
 
-import qualified CSExpr as TE
+import qualified CSExpr as TE -- (Typed) Target expressions
+import qualified UntypedCSExpr as UE  -- Untyped target expressions
 
-erase :: TE.Expr -> IO TE.Expr
-erase pgm = return pgm
+erase :: TE.Expr -> IO UE.Expr
+erase (TE.ValExpr v) = do
+  uv <- eraseVal v
+  return (UE.ValExpr uv)
+
+
+eraseVal :: TE.Value -> IO UE.Value
+eraseVal (TE.Lit lit) = return (UE.Lit lit)
 
