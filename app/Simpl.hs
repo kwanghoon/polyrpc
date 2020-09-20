85a3ec22bf3d3722211d16b37f11495cf440e4f9
Kwanghoon Choi <lazyswamp@gmail.com>
2020-09-20 23:58:58 +0900

    Restructured the Simplification

--- app/Simpl.hs
+++ app/Simpl.hs
@@ -30,20 +30,10 @@ initUseInfo = UseInfo { cNameUseInfo = empty, varUseInfo = empty }
 simpl :: Monad m => GlobalTypeInfo -> FunctionStore -> Expr
                      -> m (GlobalTypeInfo, FunctionStore, Expr)
 simpl gti funStore mainexpr = do
-  useInfo <-useAnalysis funStore mainexpr
-  (funStore', mainexpr') <- doSimpl useInfo funStore mainexpr
-  return (gti, funStore, mainexpr')
-
---------------------------------------------------------------------------------
--- Use analysis
---------------------------------------------------------------------------------
-useAnalysis :: Monad m => FunctionStore -> Expr -> m UseInfo
-useAnalysis funStore expr = do
   let initUseInfo = initUseInfoFrom funStore
-  useInfo1 <- uaExpr initUseInfo expr
-  useInfo2 <- uaFunStore useInfo1 funStore
-  return useInfo2
-
+  (useInfo, mainexpr') <-doExpr initUseInfo mainexpr
+  funStore' <- doFunStore useInfo funStore
+  return (gti, funStore', mainexpr')
 
 initUseInfoFrom :: FunctionStore -> UseInfo
 initUseInfoFrom funStore = UseInfo
@@ -53,15 +43,49 @@ initUseInfoFrom funStore = UseInfo
         [ codeName | (codeName, _) <- _serverstore funStore ])
   , varUseInfo = empty }
 
+--
+doExpr :: Monad m => UseInfo -> Expr -> m (UseInfo, Expr)
+doExpr useInfo (ValExpr v) = do
+  (useInfo', v') <- doValue useInfo v
+  return (useInfo', ValExpr v')
+doExpr useInfo (Let bindDecls expr) =
+  return (useInfo, Let bindDecls expr)
+doExpr useInfo (App v ty arg) =
+  return (useInfo, App v ty arg)
+doExpr useInfo (TypeApp v ty tys) =
+  return (useInfo, TypeApp v ty tys)
+doExpr useInfo (LocApp v ty locs) =
+  return (useInfo, LocApp v ty locs)
+doExpr useInfo (Prim op locs tys vs) =
+  return (useInfo, Prim op locs tys vs)
 
-uaFunStore :: Monad m => UseInfo -> FunctionStore -> m UseInfo
-uaFunStore useInfo funStore = return useInfo
+--
+doValue :: Monad m => UseInfo -> Value -> m (UseInfo, Value)
+doValue useInfo (Var x) =
+  return (useInfo, Var x)
+doValue useInfo (Lit x) =
+  return (useInfo, Lit x)
+doValue useInfo (Tuple vs) =
+  return (useInfo, Tuple vs)
+doValue useInfo (Constr cname locs tys vs tys') =
+  return (useInfo, Constr cname locs tys vs tys')
+doValue useInfo (Closure vs tys cname optrecs) =
+  return (useInfo, Closure vs tys cname optrecs)
+doValue useInfo (UnitM v) =
+  return (useInfo, UnitM v)
+doValue useInfo (BindM bindDecls expr) =
+  return (useInfo, BindM bindDecls expr)
+doValue useInfo (Req v ty arg) =
+  return (useInfo, Req v ty arg)
+doValue useInfo (Call v ty arg) =
+  return (useInfo, Call v ty arg)
+doValue useInfo (GenApp loc v ty arg) =
+  return (useInfo, GenApp loc v ty arg)
 
-uaExpr :: Monad m => UseInfo -> Expr -> m UseInfo
-uaExpr useInfo expr = return useInfo
+--
+doFunStore :: Monad m => UseInfo -> FunctionStore -> m FunctionStore
+doFunStore useInfo funStore =
+  -- 1. do Use Analysis
+  -- 2. Delete unused codes
+  return funStore
 
---------------------------------------------------------------------------------
--- Simplification
---------------------------------------------------------------------------------
-doSimpl :: Monad m => UseInfo -> FunctionStore -> Expr -> m (FunctionStore, Expr)
-doSimpl useInfo funstore expr = return (funstore, expr)
