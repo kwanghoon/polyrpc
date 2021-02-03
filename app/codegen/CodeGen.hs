6e50cc355138db90c155967dbac2454eb81ea77e
Kwanghoon Choi <lazyswamp@gmail.com>
2021-02-03 23:44:29 +0000

    Refacotred cgExpr, cgVal, and cgLoc in CodeGen; Add mem and prim funcs to Runtime

--- app/codegen/CodeGen.hs
+++ app/codegen/CodeGen.hs
@@ -13,12 +13,12 @@ codeGen t_gti funStore t_expr = return ()
 
 -- | function map compilation
 
-compFunMap :: String -> CS.FunctionMap -> R.FunctionMap
-compFunMap myloc csFunMap = funMap
+cgFunMap :: String -> CS.FunctionMap -> R.FunctionMap
+cgFunMap myloc csFunMap = funMap
   where
-    funMap = compFunMap' myloc csFunMap funMap
+    funMap = cgFunMap' myloc csFunMap funMap
 
-    compFunMap' myloc csFunMap funMap =
+    cgFunMap' myloc csFunMap funMap =
       [ (f, R.Code fvvars (R.CodeAbs ys action))
       | (f, (_, CS.Code locvars tyvars xs opencode)) <- csFunMap,
         let fvvars = locvars ++ xs,
@@ -26,145 +26,195 @@ compFunMap myloc csFunMap = funMap
       ]
 
     codeAbsToPair myloc (CS.CodeAbs xTys@[(x,_)] expr) =
-      (map fst xTys, \env v -> interp myloc funMap (env ++ [(x,v)]) expr)
+      let actionExpr = cgExpr myloc expr in
+      (map fst xTys, \env v -> actionExpr funMap (env ++ [(x,v)]))
     codeAbsToPair myloc (CS.CodeAbs xTys expr) =
-      error $ "compFuNMap: not a single arg var: " ++ show (length xTys)
+      error $ "cgFunMap: not a single arg var: " ++ show (length xTys)
        
     codeAbsToPair myloc (CS.CodeLocAbs locvars@[locvar] expr) =
-      (locvars, \env v -> interp myloc funMap (env ++ [(locvar,v)]) expr)
+      let actionExpr = cgExpr myloc expr in
+      (locvars, \env v -> actionExpr funMap (env ++ [(locvar,v)]))
     codeAbsToPair myloc (CS.CodeLocAbs locvars expr) =
-      error $ "compFuNMap: not a single loc var: " ++ show (length locvars)
+      error $ "cgFunMap: not a single loc var: " ++ show (length locvars)
 
 
+-- | cgExpr
 
--- | interp
+cgExpr :: String -> CS.Expr -> R.FunctionMap -> R.Env -> IO R.Value  -- String ==> Location name
 
-interp :: String -> R.FunctionMap -> R.Env -> CS.Expr -> IO R.Value  -- String ==> Location name
+cgExpr myloc (CS.ValExpr (CS.UnitM v)) =
+  let f = cgVal v
+  in  (\ funMap env -> return $ f funMap env)
 
-interp myloc funMap env (CS.ValExpr (CS.UnitM v)) =
-  return $ interpVal funMap env v
+cgExpr myloc (CS.ValExpr (CS.BindM [CS.Binding _ x _ bexpr] expr)) =
+  let actionBexpr = cgExpr myloc bexpr
+      actionExpr = cgExpr myloc expr
+  in  (\ funMap env -> do
+           bvalue <- actionBexpr funMap env
+           actionExpr funMap (env ++ [(x,bvalue)]) )
+
+cgExpr myloc (CS.ValExpr (CS.BindM bindDecls expr)) =
+  error $ "cgExpr: not a single binding in BindM: " ++ show (length bindDecls)
   
-interp myloc funMap env (CS.ValExpr (CS.BindM [CS.Binding _ x _ bexpr] expr)) = do
-  bvalue <- interp myloc funMap env bexpr
-  interp myloc funMap (env ++ [(x,bvalue)]) expr
-
-interp myloc funMap env (CS.ValExpr (CS.BindM bindDecls expr)) =
-  error $ "interp: not a single binding in BindM: " ++ show (length bindDecls)
+cgExpr myloc (CS.ValExpr (CS.Req f _ arg)) =
+  let actionF = cgVal f
+      actionArg = cgVal arg
+  in  (\ funMap env -> do
+          let _f   = actionF funMap env
+          let _arg = actionArg funMap env
+          R.req funMap _f _arg )
   
-interp myloc funMap env (CS.ValExpr (CS.Req f _ arg)) = do
-  let _f   = interpVal funMap env f
-  let _arg = interpVal funMap env arg
-  R.req funMap _f _arg
+cgExpr myloc (CS.ValExpr (CS.Call f _ arg)) =
+  let actionF = cgVal f
+      actionArg = cgVal arg
+  in  (\ funMap env -> do
+          let _f = actionF funMap env
+          let _arg = actionArg funMap env
+          R.call funMap _f _arg )
   
-interp myloc funMap env (CS.ValExpr (CS.Call f _ arg)) = do
-  let _f   = interpVal funMap env f
-  let _arg = interpVal funMap env arg
-  R.call funMap _f _arg
-
-interp myloc funMap env (CS.ValExpr (CS.GenApp loc f _ arg)) = do
-  let _loc = interpLoc env loc
-  let _f   = interpVal funMap env f
-  let _arg = interpVal funMap env arg
-  if myloc == clientLocName then
-    case _loc of
-        R.Constr fLoc [] ->
-          if myloc == fLoc then  R.apply funMap _f _arg
-          else R.req funMap _f _arg
-        _ -> error $ "interp: invalid location: " ++ show _loc
+cgExpr myloc (CS.ValExpr (CS.GenApp loc f _ arg)) =
+  let actionF = cgVal f
+      actionArg = cgVal arg
+      actionLoc = cgLoc loc 
+  in  (\ funMap env -> do
+          let _f = actionF funMap env
+          let _arg = actionArg funMap env
+          let _loc = actionLoc env
   
-  else if myloc == serverLocName then
-    case _loc of
-        R.Constr fLoc [] ->
-          if myloc == fLoc then  R.apply funMap _f _arg
-          else R.call funMap _f _arg
-        _ -> error $ "interp: invalid location: " ++ show _loc
+          if myloc == clientLocName then
+           case _loc of
+               R.Constr fLoc [] ->
+                 if myloc == fLoc then  R.apply funMap _f _arg
+                 else R.req funMap _f _arg
+               _ -> error $ "cgExpr: invalid location: " ++ show _loc
   
-  else error $ "interp: invalid location: " ++ myloc
-
-interp myloc funMap env (CS.Let [CS.Binding _ x _ bexpr] expr) = do
-  v <- interp myloc funMap env bexpr
-  interp myloc funMap (env ++ [(x, v)]) expr
-
-interp myloc funMap env (CS.Let bindDecls expr) = 
-  error $ "interp: not a single binding in Let: " ++ show (length bindDecls)
-
-interp myloc funMap env (CS.Case v _ alts) = do
-  let _v = interpVal funMap env v
-  case _v of
-    R.Constr cname args ->
-      case [ alt | alt@(CS.Alternative dname _ _) <- alts, cname==dname ] of
-        [CS.Alternative _ xs aexpr]
-          | length args == length xs -> interp myloc funMap (env ++ zip xs args) aexpr
-          | otherwise -> error $ "interp: No matching alternative for: "
-                            ++ cname ++ show (length args) ++ " != " ++ show (length xs)
-        _ -> error $ "interp: No matching alternative for: " ++ cname
-
-    R.Tuple args ->
-      case alts of
-        [CS.TupleAlternative xs aexpr]
-          | length args == length xs -> interp myloc funMap (env ++ zip xs args) aexpr
-          | otherwise -> error $ "interp: No matching tuple alternative: "
-                            ++ show (length args) ++ " != " ++ show (length xs)
-        _ -> error $ "interp: Must be TupleAlternative but not: " ++ show alts
-
-    _ -> error $ "interp: Must be Constr but not: " ++ show v
-
-
-interp myloc funMap env (CS.App f _ arg) = do
-  let _f = interpVal funMap env f
-  let _arg = interpVal funMap env arg
-  R.apply funMap _f _arg
+          else if myloc == serverLocName then
+           case _loc of
+               R.Constr fLoc [] ->
+                 if myloc == fLoc then  R.apply funMap _f _arg
+                 else R.call funMap _f _arg
+               _ -> error $ "cgExpr: invalid location: " ++ show _loc
   
-interp myloc funMap env (CS.TypeApp f _ tyargs) = do
-  let _f = interpVal funMap env f
-  return _f
+          else error $ "cgExpr: invalid location: " ++ myloc )
 
-interp myloc funMap env (CS.LocApp f _ locargs) = do
-  let _f = interpVal funMap env f
-  let _locargs = map (interpLoc env) locargs
-  foldl (\ faction locval -> do
-           fval <- faction
-           R.apply funMap fval locval) (return _f) _locargs
+cgExpr myloc (CS.Let [CS.Binding _ x _ bexpr] expr) =
+  let actionBexpr = cgExpr myloc bexpr
+      actionExpr = cgExpr myloc expr
+  in (\ funMap env -> do
+          v <- actionBexpr funMap env
+          actionExpr funMap (env ++ [(x, v)]) )
 
-interp myloc funMap env (CS.Prim op locargs _ args) = do
-  let _locargs = map (interpLoc env) locargs
-  let _args = map (interpVal funMap env) args
-  R.prim op _locargs _args
+cgExpr myloc (CS.Let bindDecls expr) = 
+  error $ "cgExpr: not a single binding in Let: " ++ show (length bindDecls)
 
+cgExpr myloc (CS.Case v _ alts) =
+  let actionV = cgVal v
+      actionAlts = cgAlts myloc alts
+  in  (\ funMap env -> do
+           let _v = actionV funMap env
+           actionCase _v actionAlts funMap env )
+  where
+    cgAlts myloc alts =
+       [ case alt of
+           CS.Alternative dname xs expr -> (Just dname, xs, cgExpr myloc expr)
+           CS.TupleAlternative xs expr -> (Nothing, xs, cgExpr myloc expr)
+       | alt <- alts ]
 
--- | interpVal
+    actionCase v@(R.Constr cname args) actionAlts funMap env =
+      case [(xs, actionExpr)
+           | (Just dname, xs, actionExpr) <- actionAlts, cname==dname] of
+        ((xs, actionExpr):_)
+          | length args == length xs -> actionExpr funMap (env ++ zip xs args)
+          | otherwise -> error $ "cgExpr: arity mismatch in an alternative: "
+                           ++ show (length args) ++ " != " ++ show (length xs)
+        [] -> error $ "cgExpr: No matching alternative for: " ++ show v
 
-interpVal :: R.FunctionMap -> R.Env -> CS.Value -> R.Value
-interpVal funMap env (CS.Var x) =
+    actionCase (R.Tuple args) [(Nothing, xs, actionExpr)] funMap env
+      | length args == length xs = actionExpr funMap (env ++ zip xs args)
+      | otherwise = error $ "cgExpr: arity mismatch in a tuple alternative: "
+                               ++ show (length args) ++ " != " ++ show (length xs)
+       
+    actionCase (R.Tuple args) _ funMap env =
+      error $ "cgExpr: Not a single tuple alternative"
+      
+    actionCase v actionAlts funMap env =
+      error $ "cgExpr: No matching alternative for: " ++ show v
+
+cgExpr myloc (CS.App f _ arg) =
+  let actionF = cgVal f
+      actionArg = cgVal arg
+  in  (\ funMap env -> do
+          let _f = actionF funMap env
+          let _arg = actionArg funMap env
+          R.apply funMap _f _arg )
+  
+cgExpr myloc (CS.TypeApp f _ tyargs) =
+  let actionF = cgVal f
+  in  (\ funMap env -> do
+          let _f = actionF funMap env
+          return _f )
+
+cgExpr myloc (CS.LocApp f _ locargs) =
+  let actionF = cgVal f
+      actionLocargs = map cgLoc locargs
+  in  (\ funMap env -> do
+           let _f = actionF funMap env
+           let _locargs = map (\f -> f env) actionLocargs
+           foldl (\ action locval -> do
+                fval <- action
+                R.apply funMap fval locval) (return _f) _locargs )
+
+cgExpr myloc (CS.Prim op locargs _ args) =
+  let actionLocargs = map cgLoc locargs
+      actionArgs = map cgVal args
+  in  (\ funMap env -> do
+           let _locargs = map (\f -> f env) actionLocargs
+           let _args = map (\f -> f funMap env) actionArgs
+           R.prim op _locargs _args )
+
+-- | cgVal
+
+cgVal :: CS.Value -> R.FunctionMap -> R.Env -> R.Value
+cgVal (CS.Var x) = \funMap env ->
   case [ v | (y,v) <- env, x==y ] of
     (v:_) -> v
-    [] -> error $ "interpVal: not found: " ++ x
+    [] -> error $ "cgVal: not found: " ++ x
 
-interpVal funMap env (CS.Lit lit) = R.Lit lit
+cgVal (CS.Lit lit) = \funMap env -> R.Lit lit
 
-interpVal funMap env (CS.Tuple vs) = R.Tuple (map (interpVal funMap env) vs)
+cgVal (CS.Tuple vs) =
+  let fs = map cgVal vs in
+  (\ funMap env -> let runtime_vs = map (\f -> f funMap env) fs in R.Tuple runtime_vs )
+    
 
-interpVal funMap env (CS.Constr c locargs _ args _) =
-  R.Constr c $ map (interpLoc env) locargs ++ map (interpVal funMap env) args
+cgVal (CS.Constr c locargs _ args _) =
+  let fs = map cgLoc locargs 
+      gs = map cgVal args
+  in (\ funMap env ->
+          let runtime_vs = map (\f -> f env) fs
+              runtime_ws = map (\g -> g funMap env) gs
+          in  R.Constr c $ runtime_vs ++ runtime_ws )
 
-interpVal funMap env (CS.Closure freevals _ (CS.CodeName f locs _) optrecf) = 
-  let _freevals = map (interpVal funMap env) freevals
-      _locvals = map (interpLoc env) locs
-  in  R.Closure (_locvals ++ _freevals) (R.CodeName f) optrecf
+cgVal (CS.Closure freevals _ (CS.CodeName f locs _) optrecf) =
+  let fs = map cgVal freevals
+      gs = map cgLoc locs
+  in  (\ funMap env ->
+           let runtime_vs = map (\f -> f funMap env) fs
+               runtime_ws = map (\g -> g env) gs
+           in  R.Closure (runtime_ws ++ runtime_vs) (R.CodeName f) optrecf ) -- Note: locvals first!!
 
+-- | cgLoc
 
--- | interpLoc
+cgLoc :: Location -> R.Env -> R.Value
 
-interpLoc :: R.Env -> Location -> R.Value
+cgLoc (Location constloc) 
+  | constloc == clientLocName = \env -> R.Constr clientLocName []
+  | constloc == serverLocName = \env -> R.Constr serverLocName []
+  | otherwise = error $ "cgLoc: not found: " ++ constloc
 
-interpLoc env (Location constloc)
-  | constloc == clientLocName = R.Constr clientLocName []
-  | constloc == serverLocName = R.Constr serverLocName []
-  | otherwise = error $ "interpLoc: not found: " ++ constloc
-
-interpLoc env (LocVar x) =
+cgLoc (LocVar x) = \env ->
   case [ z | (y,z) <- env, x==y ] of
     (w:_) -> w
-    [] -> error $ "interpLoc: not found: " ++ x
+    [] -> error $ "cgLoc: not found: " ++ x
 
 
--- app/codegen/Runtime.hs
+++ app/codegen/Runtime.hs
@@ -5,6 +5,7 @@ module Runtime where
 import Literal
 import Prim
 
+import qualified Data.Map as Map
 import Text.JSON.Generic
 
 data Value =
@@ -98,8 +99,126 @@ loop_server funMap = do
       w <- apply funMap f arg
       send (Constr "UNIT" [w])
       loop_req funMap
-      
+
+-- | Memory
+
+data Mem = Mem { _new :: Integer, _map :: Map.Map Addr Value }
+
+type Addr = Integer
+
+initMem = Mem { _new=1, _map=Map.empty }
+
+allocMem :: Value -> Mem -> (Addr, Mem)
+allocMem v mem =
+  let next = _new mem
+      addrVals = _map mem
+  in  (next, mem { _new=next+1, _map=Map.insert next v addrVals })
+
+readMem :: Addr -> Mem -> Value
+readMem addr mem =
+  case Map.lookup addr (_map mem) of
+   Just v -> v
+   Nothing -> error $ "[readMem] Not found: " ++ show addr
+
+writeMem :: Addr -> Value -> Mem -> Mem
+writeMem addr v mem = mem { _map= Map.insert addr v (_map mem) }
+
+
 --
 prim :: PrimOp -> [Value] -> [Value] -> IO Value
 prim op locvals argvals = return $ Tuple []  -- Tofix
+
+-- calc :: PrimOp -> [Location] -> [Type] -> [Value] -> Mem -> IO (Value, Mem)
+
+-- -- calc MkRecOp locs tys [Closure vs fvtys codename [], Lit (StrLit f)] mem =
+-- --   return (Closure vs fvtys codename [f], mem)
+
+-- -- calc MkRecOp locs tys vs mem =
+-- --   error $ "[MkRecOp]: Unexpected: "
+-- --               ++ show locs ++ " " ++ show tys ++ " " ++ show vs
+
+-- calc PrimRefCreateOp [loc1] [ty] [v] mem =
+--   let (addr, mem1) = allocMem v mem in return (Addr addr, mem1)
+
+-- calc PrimRefCreateOp locs tys vs mem =
+--   error $ "[PrimOp] PrimRefCreateOp: Unexpected: "
+--               ++ show locs ++ " " ++ show  tys ++ " " ++ show vs
+
+-- calc PrimRefReadOp [loc1] [ty] [Addr addr] mem = return (readMem addr mem, mem)
+
+-- calc PrimRefReadOp locs tys vs mem =
+--   error $ "[PrimOp] PrimRefReadOp: Unexpected: "
+--               ++ show locs ++ " " ++ show  tys ++ " " ++ show vs
+
+-- calc PrimRefWriteOp [loc1] [ty] [Addr addr,v] mem =
+--   return (Lit UnitLit, writeMem addr v mem)
+
+-- calc PrimRefWriteOp locs tys vs mem =
+--   error $ "[PrimOp] PrimRefWriteOp: Unexpected: "
+--               ++ show locs ++ " " ++ show  tys ++ " " ++ show vs
+
+-- calc PrimReadOp [loc] [] [Lit (UnitLit)] mem = do
+--   line <- getLine
+--   return (Lit (StrLit line), mem)
+
+-- calc PrimPrintOp [loc] [] [Lit (StrLit s)] mem = do
+--   putStr s
+--   return (Lit UnitLit, mem)
+
+
+-- calc primop locs tys vs mem =
+--   return (Lit $ calc' primop locs tys (map getLit vs), mem)
+
+--   where
+--     getLit (Lit lit) = lit
+--     getLit v = error $ "[Execute] calc-getLit: what? " ++ show v
+--                        ++ "\n  in " ++ show (Prim primop locs tys vs)
+
+-- -- Primitives
+-- calc' :: PrimOp -> [Location] -> [Type] -> [Literal] -> Literal
+
+-- calc' NotPrimOp [loc] [] [BoolLit b] = BoolLit (not b)  -- loc is the current location
+
+-- calc' OrPrimOp [loc] [] [BoolLit x, BoolLit y] = BoolLit (x || y)
+
+-- calc' AndPrimOp [loc] [] [BoolLit x, BoolLit y] = BoolLit (x && y)
+
+-- calc' EqStringPrimOp [loc] [] [StrLit x, StrLit y] = BoolLit (x==y)
+
+-- calc' EqBoolPrimOp [loc] [] [BoolLit x, BoolLit y] = BoolLit (x==y)
+
+-- calc' EqIntPrimOp [loc] [] [IntLit x, IntLit y] = BoolLit (x==y)
+
+-- calc' NeqStringPrimOp [loc] [] [StrLit x, StrLit y] = BoolLit (x/=y)
+
+-- calc' NeqBoolPrimOp [loc] [] [BoolLit x, BoolLit y] = BoolLit (x/=y)
+
+-- calc' NeqIntPrimOp [loc] [] [IntLit x, IntLit y] = BoolLit (x/=y)
+
+-- calc' LtPrimOp [loc] [] [IntLit x, IntLit y] = BoolLit (x<y)
+
+-- calc' LePrimOp [loc] [] [IntLit x, IntLit y] = BoolLit (x<=y)
+
+-- calc' GtPrimOp [loc] [] [IntLit x, IntLit y] = BoolLit (x>y)
+
+-- calc' GePrimOp [loc] [] [IntLit x, IntLit y] = BoolLit (x>=y)
+
+-- calc' AddPrimOp [loc] [] [IntLit x, IntLit y] = IntLit (x+y)
+
+-- calc' SubPrimOp [loc] [] [IntLit x, IntLit y] = IntLit (x-y)
+
+-- calc' MulPrimOp [loc] [] [IntLit x, IntLit y] = IntLit (x*y)
+
+-- calc' DivPrimOp [loc] [] [IntLit x, IntLit y] = IntLit (x `div` y)
+
+-- calc' NegPrimOp [loc] [] [IntLit x] = IntLit (-x)
+
+-- -- Libraries
+-- calc' PrimIntToStringOp [loc] [] [IntLit i] = StrLit (show i)
+
+-- calc' PrimConcatOp [loc] [] [StrLit s1, StrLit s2] = StrLit (s1++s2)
+
+-- calc' operator locs tys operands =
+--   error $ "[PrimOp] Unexpected: "
+--      ++ show operator ++ " " ++ show locs ++ " " ++ show tys ++ " " ++ show operands
 
