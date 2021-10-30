00435caf169afa0dd0379e16a2405612fa1b7147
Kwanghoon Choi <lazyswamp@gmail.com>
2021-10-30 17:04:33 +0100

    temporary yapb dependence; syntax completion

---
+++ app/syncomp/SyntaxCompletion.hs
@@ -0,0 +1,52 @@
+module SyntaxCompletion (computeCand) where
+
+import CommonParserUtil 
+
+import Expr
+
+import TokenInterface
+import Terminal
+import Lexer (lexerSpec)
+import Parser (parserSpec)
+import System.IO
+
+-- for syntax completion
+import Token
+import SynCompInterface
+import Control.Exception
+import Data.Typeable
+
+-- Todo: The following part should be moved to the library.
+--       Arguments: lexerSpec, parserSpec
+--                  isSimpleMode
+--                  programTextUptoCursor, programTextAfterCursor
+
+maxLevel = 10000
+
+-- | computeCand
+computeCand :: Bool -> String -> String -> Bool -> IO [EmacsDataItem]
+computeCand debug programTextUptoCursor programTextAfterCursor isSimpleMode = (do
+  {- 1. Lexing  -}                                                                         
+  (line, column, terminalListUptoCursor)  <-
+    lexingWithLineColumn lexerSpec 1 1 programTextUptoCursor
+
+  {- 2. Parsing -}
+  ((do ast <- parsing debug parserSpec terminalListUptoCursor
+       successfullyParsed)
+
+    `catch` \parseError ->
+      case parseError :: ParseError Token AST of
+        _ ->
+          {- 3. Lexing the rest and computing candidates with it -}
+          do (_, _, terminalListAfterCursor) <-
+               lexingWithLineColumn lexerSpec line column programTextAfterCursor
+             handleParseError
+               (HandleParseError {
+                   debugFlag=debug,
+                   searchMaxLevel=maxLevel,
+                   simpleOrNested=isSimpleMode,
+                   postTerminalList=terminalListAfterCursor,
+                   nonterminalToStringMaybe=Nothing})
+               parseError))
+
+  `catch` \lexError ->  case lexError :: LexError of  _ -> handleLexError
